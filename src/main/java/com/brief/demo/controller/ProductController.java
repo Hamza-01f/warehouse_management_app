4baa32de4461a493f6efef7676047033d494e97a
hamza <hamzaprograme@gmail.com>
2025-12-20 23:27:16 +0000

    adding method level authorities

--- src/main/java/com/brief/demo/controller/ProductController.java
+++ src/main/java/com/brief/demo/controller/ProductController.java
@@ -9,6 +9,7 @@ import com.brief.demo.service.ProductService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,30 +23,35 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
+    @PreAuthorize("hasAuthority('PRODUCT_CREATE')")
     public ResponseEntity<ProductResponseDTO> createProduct( @RequestBody ProductRequestDTO request) {
         ProductResponseDTO response = productService.createProduct(request);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('PRODUCT_READ')")
     public ResponseEntity<List<ProductResponseDTO>> getAllProducts() {
         List<ProductResponseDTO> products = productService.getAllProducts();
         return ResponseEntity.ok(products);
     }
 
     @GetMapping("/active")
+    @PreAuthorize("hasAuthority('PRODUCT_READ_ACTIVE')")
     public ResponseEntity<List<ProductResponseDTO>> getActiveProducts() {
         List<ProductResponseDTO> products = productService.getActiveProducts();
         return ResponseEntity.ok(products);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('PRODUCT_READ')")
     public ResponseEntity<ProductResponseDTO> getProductById(@PathVariable Long id) {
         ProductResponseDTO product = productService.getProductById(id);
         return ResponseEntity.ok(product);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('PRODUCT_UPDATE')")
     public ResponseEntity<ProductResponseDTO> updateProduct(
             @PathVariable Long id,
             @RequestBody ProductRequestUpdateDTO request) {
@@ -54,12 +60,14 @@ public class ProductController {
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('PRODUCT_DELETE')")
     public ResponseEntity<ApiResponseDTO> deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
         return ResponseEntity.ok(new ApiResponseDTO("Product deleted successfully", true));
     }
 
     @PatchMapping("/{id}/activate")
+    @PreAuthorize("hasAuthority('PRODUCT_UPDATE')")
     public ResponseEntity<ApiResponseDTO> activateProduct(@PathVariable Long id) {
         productService.activateProduct(id);
         return ResponseEntity.ok(new ApiResponseDTO("Product activated successfully", true));
